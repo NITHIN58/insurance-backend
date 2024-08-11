4a0e3ee09e3f215c661eddf9bd21ae36fdcb07ea
NITHIN58 <nithinbiju58@gmail.com>
2024-08-12 00:11:06 +0530

    cpuUtlizeTempcomment

--- server.js
+++ server.js
@@ -33,13 +33,13 @@ process.on('unhandledRejection', err => {
 
 
 //Track real-time CPU utilization of the node server and on 70% usage restart the server.
-setInterval(() => {
-    os.cpuUsage((v) => {
-        console.log("CPU Usage: " + (v * 100).toFixed(2) + "%");
-        if ((v * 100).toFixed(2) > 70) {
-            console.log('CPU Usage is high. Restarting...');
-            process.exit(0);
+// setInterval(() => {
+//     os.cpuUsage((v) => {
+//         console.log("CPU Usage: " + (v * 100).toFixed(2) + "%");
+//         if ((v * 100).toFixed(2) > 70) {
+//             console.log('CPU Usage is high. Restarting...');
+//             process.exit(0);
             
-        }
-    });
-}, 10000)
+//         }
+//     });
+// }, 10000)
